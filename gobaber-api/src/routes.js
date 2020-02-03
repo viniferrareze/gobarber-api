683a54c15db46ab5b7d3d8646430bd9fe90b6b77
carlos vinícius ferrareze <viniferrareze@gmail.com>
2020-02-02 23:35:09 -0300

    feat: criando cadastro de usuário

--- gobaber-api/src/routes.js
+++ gobaber-api/src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'vini',
-    email: 'viniferrareze@gmail.com',
-    password_hash: '123321',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
