a4de9bb069fc6d97983f090f979ba66df6a305be
carlos vinícius ferrareze <viniferrareze@gmail.com>
2020-02-10 14:17:41 -0300

    feat: notificando novos agendamentos

--- gobaber-api/src/app/controllers/AppointmentController.js
+++ gobaber-api/src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
    async index(req, res) {
@@ -80,10 +82,24 @@ class AppointmentController {
             .json({ error: 'Appoint date is not available' });
       }
 
+      // cria o agendamento
       const appointment = await Appointment.create({
          user_id: req.userId,
          provider_id,
          date: hourStart,
+      });
+
+      const user = await User.findByPk(req.userId);
+      const formattedDate = format(
+         hourStart,
+         "'dia' dd 'de' MMMM', às' H:mm'h' ",
+         { locale: pt }
+      );
+
+      // cria a notificação
+      await Notification.create({
+         content: `Novo agendamento de ${user.name} para dia ${formattedDate}`,
+         user: provider_id,
       });
 
       return res.json(appointment);
