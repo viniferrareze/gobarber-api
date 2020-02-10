c677a08b99787f4398d42967fb89fb7fb6045b90
carlos vinícius ferrareze <viniferrareze@gmail.com>
2020-02-10 13:37:38 -0300

    feat: listagem de agenda do prestador de serviço

--- gobaber-api/src/routes.js
+++ gobaber-api/src/routes.js
@@ -9,6 +9,7 @@ import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middleware/auth';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 
@@ -25,6 +26,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointment', AppointmentController.store);
 routes.get('/appointment', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
