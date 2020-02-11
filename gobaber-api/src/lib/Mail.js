bcb168990c0f92b1ac5b0a0e891478b5d1b20b27
carlos vinícius ferrareze <viniferrareze@gmail.com>
2020-02-11 13:00:51 -0300

    feat: Configurando templates de e-mails.

--- gobaber-api/src/lib/Mail.js
+++ gobaber-api/src/lib/Mail.js
@@ -1,4 +1,7 @@
 import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
 
 import mailConfig from '../config/mail';
 
@@ -12,6 +15,26 @@ class Mail {
          secure,
          auth: auth.user ? auth : null, // se existir o usuário passa o mesmo se nao envia null
       });
+
+      this.configTemplates();
+   }
+
+   configTemplates() {
+      const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+      this.transporter.use(
+         'compile', // como ele compila os templates de emails
+         nodemailerhbs({
+            viewEngine: exphbs.create({
+               layoutsDir: resolve(viewPath, 'layouts'),
+               partialsDir: resolve(viewPath, 'partials'),
+               defaultLayout: 'default',
+               extname: '.hbs',
+            }),
+            viewPath,
+            extName: '.hbs',
+         })
+      );
    }
 
    sendMail(message) {
