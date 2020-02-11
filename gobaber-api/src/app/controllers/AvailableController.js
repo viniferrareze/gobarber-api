8f5f11806002bec22e5037cef3291822643dc4ab
carlos vinícius ferrareze <viniferrareze@gmail.com>
2020-02-11 17:58:52 -0300

    feat: Listando horarios disponiveis

---
+++ gobaber-api/src/app/controllers/AvailableController.js
@@ -0,0 +1,72 @@
+import {
+   startOfDay,
+   endOfDay,
+   setHours,
+   setMinutes,
+   setSeconds,
+   format,
+   isAfter,
+} from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+
+class AvailableController {
+   async index(req, res) {
+      const { date } = req.query;
+
+      // verifica se veio o date
+      if (!date) {
+         return res.status(400).json({ error: 'Invalid date!' });
+      }
+
+      // garante a data por inteiro
+      const searchData = Number(date);
+
+      const appointments = await Appointment.findAll({
+         where: {
+            provider_id: req.params.providerId,
+            canceled_at: null,
+            date: {
+               [Op.between]: [startOfDay(searchData), endOfDay(searchData)],
+            },
+         },
+      });
+
+      const schedule = [
+         '08:00',
+         '09:00',
+         '10:00',
+         '11:00',
+         '12:00',
+         '13:00',
+         '14:00',
+         '15:00',
+         '16:00',
+         '17:00',
+         '18:00',
+         '19:00',
+         '20:00',
+      ];
+
+      const avaiable = schedule.map(time => {
+         const [hours, minute] = time.split(':');
+
+         const value = setSeconds(
+            setMinutes(setHours(searchData, hours), minute),
+            0
+         );
+
+         return {
+            time,
+            value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+            avaiable:
+               isAfter(value, new Date()) &&
+               !appointments.find(a => format(a.date, 'HH:mm') === time),
+         };
+      });
+
+      return res.json(avaiable);
+   }
+}
+
+export default new AvailableController();
--- gobaber-api/src/routes.js
+++ gobaber-api/src/routes.js
@@ -10,6 +10,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 
@@ -23,6 +24,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.post('/appointment', AppointmentController.store);
 routes.get('/appointment', AppointmentController.index);
